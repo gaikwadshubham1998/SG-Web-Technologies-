f656967dbd72f1999ca1f8cbfbc67690b44f3be9
sgweb2025e <shubhamgaikwad7258@gmail.com>
2026-01-23 06:25:38 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,22 +51,7 @@ function eraseText() {
     }
 }
 
-// ios hero video 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const video = document.querySelector(".hero-video");
-
-    const isIphone = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-
-    if (isIphone && video) {
-        video.setAttribute("webkit-playsinline", "");
-        video.muted = true;
-        video.play().catch(() => {
-            // iOS autoplay fallback
-            video.controls = false;
-        });
-    }
-});
 
 
 
